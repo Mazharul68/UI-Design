8b948dd26c83b301452d34894a39a41328311a53
Md. Mazharul Islam <79826648+Mazharul68@users.noreply.github.com>
2022-10-07 01:51:14 +0600

    blank page added

--- Personal_UI_Design/Admin_Dashboard/assets/js/main.js
+++ Personal_UI_Design/Admin_Dashboard/assets/js/main.js
@@ -1,26 +1,9 @@
 $(document).ready(function() {
     // Navbar-menu-Active-Class Add
-    $(".main_menu > li").click(function() {
+    $(".navigation .nav_item").click(function() {
         $(this).addClass('active').siblings().removeClass('active');
     });
 
-    $(window).scroll(function() {
-        var ourWindow = $(window).scrollTop();
-
-        if (ourWindow < 500) {
-            $('.top').fadeOut(2000);
-        } else {
-            $('.top').fadeIn(2000);
-        }
-
-        /***Navbar-fixed-Start Here***/
-
-        if (ourWindow > 100) {
-            $('body').addClass('fixed');
-        } else {
-            $('body').removeClass('fixed');
-        }
-    });
 
     // toggle menu/navbar Scripts
     // $('.navigation ul.navi_items li.nav_item').on('click',function() {
@@ -41,8 +24,6 @@ $(document).ready(function() {
     //     // }
        
     // });
-
-
     menuDropdowns = function(){
         $('.nav_item').each(function(){
             const links = $(this).find('.dropdown_menu');
